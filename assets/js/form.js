a2fe417d28cdd9becda4568b04283d499b3f47d8
Asep Saepudin <57556512+aseppp@users.noreply.github.com>
2022-01-01 11:22:19 +0700

    Upload ke 5 - checkbox and responsive
    Fix fitur checkbox dan membuat responsive

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,27 +1,51 @@
+// Variable untuk email pemilik/owner
 const emailOwner = "saepudinasep86@gmail.com"
 
+// Get element menggunakan metode getElementByID
 let nameUser = document.getElementById('input-name');
 let email = document.getElementById('input-email');
 let phone = document.getElementById('input-phone');
 let chose = document.getElementById('input-subject');
-let skills = document.querySelector('.skills');
 let message = document.getElementById('input-message');
 
-
+// Function button submit pada from
 function submitData() {
+    // Untuk mengidentifikasi element yang berisi value dari input
     nameUser = nameUser.value;
     email = email.value;
     phone = phone.value;
     chose = chose.value;
     message = message.value;
-    skills = skills.checked;
 
 
+    // Kondisi untuk checkbox
+    // Variable untuk element checkbox
+    let html = document.getElementById('html');
+    let css = document.getElementById('css');
+
+    // Untuk mengidentifikasi element yang berisi value dari input
+    html = html.checked;
+    css = css.checked;
+
+    if (html) {
+        html = (document.getElementById('html').value)
+    } else {
+        html = ""
+    }
+
+    if (css) {
+        css = (document.getElementById('css').value);
+    } else {
+        css = ""
+    }
+
+
+    // Kondisi untuk check form sudah terisi semua atau belum
     if (nameUser === "" || email === "" || phone == "" || chose === "" || message === "") {
         return alert("Anda harus melengkapi data ini!")
     }
 
     let a = document.createElement('a');
-    a.href = `mailto: ${emailOwner}?subject=${chose}&body=Halo nama saya, ${nameUser} pesan saya ${message}, skill anda ${skills}`;
+    a.href = `mailto: ${emailOwner}?subject=${chose}&body=Halo nama saya, ${nameUser}, pesan saya ${message}, skill yang dibutuhkan ${html} ${css}`;
     a.click()
 }
