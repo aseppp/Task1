9fc6a054f951e09c92d5033346e037716b368974
aseppp <saepudinasep86@gmail.com>
2022-01-07 21:16:58 +0700

    add style and fixing form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,15 +1,16 @@
-// Variable untuk email pemilik/owner
-const emailOwner = "saepudinasep86@gmail.com"
-
-// Get element menggunakan metode getElementByID
-let nameUser = document.getElementById('input-name');
-let email = document.getElementById('input-email');
-let phone = document.getElementById('input-phone');
-let chose = document.getElementById('input-subject');
-let message = document.getElementById('input-message');
-
 // Function button submit pada from
 function submitData() {
+
+    // Get element menggunakan metode getElementByID
+    let nameUser = document.getElementById('input-name');
+    let email = document.getElementById('input-email');
+    let phone = document.getElementById('input-phone');
+    let chose = document.getElementById('input-subject');
+    let message = document.getElementById('input-message');
+
+     // Variable untuk email pemilik/owner
+     const emailOwner = "saepudinasep86@gmail.com"
+
     // Untuk mengidentifikasi element yang berisi value dari input
     nameUser = nameUser.value;
     email = email.value;
@@ -41,21 +42,19 @@ function submitData() {
 
 
     // Kondisi untuk check form sudah terisi semua atau belum
-    if (nameUser === "") {
+    if (nameUser == '') {
         alert("Anda harus mengisi nama")
-    } else if (email === ""){
+    } else if (email == ''){
         alert("Anda harus mengisi email")
-    } else if (phone === "") {
+    } else if (phone == '') {
         alert("Anda harus mengisi nomor telepon")
-    } else if (chose === "") {
+    } else if (chose == '') {
         alert("Anda harus mengisi subject")
-    } else if (message === "") {
+    } else if (message == '') {
         alert("Anda harus memberikan pesan")
+    } else {
+        let a = document.createElement('a');
+        a.href = `mailto: ${emailOwner}?subject=${chose}&body=Halo nama saya, ${nameUser}, pesan saya ${message}, skill yang dibutuhkan ${html} ${css}`;
+        a.click()
     }
-
-
-
-    let a = document.createElement('a');
-    a.href = `mailto: ${emailOwner}?subject=${chose}&body=Halo nama saya, ${nameUser}, pesan saya ${message}, skill yang dibutuhkan ${html} ${css}`;
-    a.click()
 }
